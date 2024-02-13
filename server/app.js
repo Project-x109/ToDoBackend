49e358c343dbef0d29cf16b764fae0b4a4b7a514
Project-x109 <amanuelgirma109@gmail.com>
2024-02-13 11:56:58 +0300

    minors

--- server/app.js
+++ server/app.js
@@ -7,9 +7,17 @@ const { logMiddleware, store } = require("./middlewares/logMiddleware.js");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const user = require("./routes/userRoutes");
+const cors = require("cors");
+const csrf = require("csurf");
 const todo = require("./routes/todoRoutes")
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "https://todo-app-mern1236.netlify.app/"],
+        credentials: true,
+    })
+);
 app.use(
     session({
         secret: process.env.SECRET,
@@ -21,10 +29,28 @@ app.use(
         },
     })
 );
-app.use(logMiddleware);
+const csrfProtection = csrf({
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+      secure: true,
+      httpOnly: true,
+    },
+    name: "X-CSRF-TOKEN",
+    value: (req) => req.csrfToken(),
+    failAction: (req, res) => {
+      res.status(403).send("Invalid CSRF token");
+    },
+  });
+  app.use(csrfProtection);
+  app.use((req, res, next) => {
+    res.cookie(req.csrfToken());
+    res.locals.csrfToken = req.csrfToken();
+    next();
+  });
 app.use(passport.initialize());
 app.use(passport.session());
 app.use("/users", user);
 app.use(todo);
+app.use(logMiddleware);
 app.use(errorHandler);
 module.exports = app;
